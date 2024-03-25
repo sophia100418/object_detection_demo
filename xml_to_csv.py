4e95b23e152819358e5ef01415f188bd5a2dec30
hopeof-GreatMind <bg.kim@sookmyung.ac.kr>
2024-03-25 15:35:52 +0900

    Modified by Dr. Kim on 24.03.25

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -6,7 +6,7 @@ python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/train -o [PATH_TO_ANNOTATIONS_FO
 # Create test data:
 python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/test -o [PATH_TO_ANNOTATIONS_FOLDER]/test_labels.csv
 """
-#Modifed by Prof. Kim om 24. 03. 24.
+#Modifed by Prof. Kim om 24. 03. 24.  It is not easy to use the Git program....!!!
 
 import os
 import glob
