682678497168c21432aa2fe54345939327ea39ce
sojeong2215969 <165887301+sophia100418@users.noreply.github.com>
2024-04-24 13:54:52 +0900

    Comment on modified date and author

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -1,16 +1,4 @@
-"""
-Usage:
-# Create train data:
-python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/train -o [PATH_TO_ANNOTATIONS_FOLDER]/train_labels.csv
-
-# Create test data:
-python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/test -o [PATH_TO_ANNOTATIONS_FOLDER]/test_labels.csv
-"""
-#Modifed by Prof. Kim om 24. 03. 24.  It is not easy to use the Git program....!!!
-#Modifed by Prof. Kim om 24. 03. 24.  It is not easy to use the Git program....!!!
-#Modifed by Prof. Kim om 24. 03. 24.  It is not easy to use the Git program....!!!#Modifed by Prof. Kim om 24. 03. 24.  It is not easy to use the Git program....!!!
-#Modifed by Prof. Kim om 24. 03. 24.  It is not easy to use the Git program....!!!
-#Modifed by Prof. Kim om 24. 03. 24.  It is not easy to use the Git program....!!!#Modifed by Prof. Kim om 24. 03. 24.  It is not easy to use the Git program....!!!
+'''Modified by Prof.Kim'''
 
 import os
 import glob
